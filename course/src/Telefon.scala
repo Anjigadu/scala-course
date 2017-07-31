a2898c2860fc6919d52d7a2d9efab9c4ca293612
Victor Rentea <victorrentea@gmail.com>
2017-07-31 15:35:46 +0300

    hashcode si equals implementate fain

--- course/src/Telefon.scala
+++ course/src/Telefon.scala
@@ -1,33 +1,25 @@
-class Telefon(cc:String, ln:String) {
-  private val countryCode: String = cc
-  private val localNumber: String = ln
-
-//class Telefon(private val countryCode:String,
-//              private val localNumber:String) {
-//  private val countryCode: String = cc
-//  private val localNumber: String = ln
+class Telefon(private val countryCode:String, private val localNumber:String) {
 
   override def equals(obj: scala.Any): Boolean =
     if (!obj.isInstanceOf[Telefon]) false
     else {
       val other = obj.asInstanceOf[Telefon]
-      this.countryCode == other.countryCode && this.localNumber == other.localNumber
+      (this.countryCode, this.localNumber) == (other.countryCode, other.localNumber)
     }
 
+//  override def hashCode(): Int = countryCode.## * 17 + localNumber.##
+  override def hashCode(): Int = (countryCode, localNumber).##
 
   override def toString: String = s"$this.countryCode $this.localNumber"
 }
-/*
-public class Telefon {
-  private final String countryCode, localNumber;
-  public Telefon(String cc, String ln) {
-    countryCode = cc;
-    localNumber = ln;
-  }
- */
 
 object TelefonPlay extends App {
   val t1 = new Telefon("+4", "0720019564")
   val t2 = new Telefon("+4", "0720019564")
   println(t1 == t2)
+
+  println("hashul t1 = " + t1.##)
+  println("hashul t2 = " + t2.##)
+  val set = Set(t1)
+  println("contine? " + set(t2)) // pura intamplare, deoarece t1## != t2##
 }
