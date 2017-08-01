2d72f0110dc34157b58682cec07e098c133ef102
Victor Rentea <victorrentea@gmail.com>
2017-08-01 14:46:54 +0300

    Chap 9 - control abstraction - refactor spre "passing a block", with higher order functions

--- course/src/Chapter9_ControlAbstraction.scala
+++ course/src/Chapter9_ControlAbstraction.scala
@@ -2,20 +2,14 @@ import java.io.File
 
 object Chapter9_ControlAbstraction extends App {
 
-  private def filesHere = (new File("course\\src")).listFiles
+  private def filesHere() = (new File("course\\src")).listFiles
 
-
-
-  def filesEnding(suffix: String):Array[File] = {
-    def matches(fileName: String) = fileName.endsWith(suffix)
-    for (file <- filesHere if matches(file.getName)) yield file
+  private def filesMatching(matches: (String) => Boolean):Array[File] = { // higher order func
+    for (file <- filesHere() if matches(file.getName)) yield file
   }
 
-  def filesContaining(bucata: String):Array[File] = {
-    def matches(fileName: String) = fileName.contains(bucata)
-    for (file <- filesHere if matches(file.getName)) yield file
-  }
-
+  def filesEnding(suffix:String) = filesMatching(_.endsWith(suffix))
+  def filesContaining(bucata:String) = filesMatching(_.contains(bucata))
 
   println(filesEnding(".scala").mkString("=="))
   println(filesContaining("Chap").mkString("--"))
