58f089e76bcf50b45f5c58c4c26732137ded4839
Victor Rentea <victorrentea@gmail.com>
2017-08-02 13:04:49 +0300

    clase ascunse

--- course/src/Chapter10_OOP.scala
+++ course/src/Chapter10_OOP.scala
@@ -2,62 +2,68 @@ import Chapter10_OOP.strings
 
 import scala.collection.mutable.ArrayBuffer
 
+object Element {
+  def apply(line:String):Element = new LineElement(line)
+  def apply(ch:Char, width:Int, height: Int):Element = new UniformElement(ch, width, height)
+  def apply(lines: Array[String]):Element = new ArrayElement(lines)
+
+  // override este OBLIGATORIU DACA suprascrii o metoda. Optional Daca o IMPLEMENTEZI
+  private class ArrayElement(initialContent: Array[String]) extends Element { // private static class
+    override val contents:Array[String] = uniformPad(initialContent)
+
+    def uniformPad(lines:Array[String]):Array[String] = {
+      val longestLine:String = lines.maxBy(_.length)
+      val maxLength:Int = longestLine.length
+
+      def padLine(line: String) = {
+        val halfNoSpaces = (maxLength - line.length) / 2
+        val halfPadding = " " * halfNoSpaces
+        halfPadding + line + halfPadding
+      }
+
+      for (line <- lines) yield padLine(line)
+    }
+
+    // implementing an abstract function fara () cu o variabila/valoare !!!!
+    // "Uniform access principle" inseamna ca implementarea unei fct abstract fara () poate fi un camp.
+    // DPDV al clientului nu ar trebui sa conteze
+    val name = "ArrayElem"
+    //def name = "ArrayElem" -- NU poti implementa un VAL ABSTRACT cu un DEF, dar poti implementa un DEF ABSTRACT cu un VAL
+  }
+
+  //new ArrayElement(Array("rosu","galben","albastru"))
+
+  private class LineElement(line:String) extends Element {
+    def contents = Array(line)
+    val name = "LineElem" // In scala final opreste override. Atat!
+    override def width = line.length
+    override def height = 1
+  }
+
+  private class UniformElement(ch:Char, width:Int, height: Int) extends Element {
+    override def contents: Array[String] = Array.fill[String](height)(ch.toString * width)
+    override val name: String = "UniformElem"
+  }
+}
+
 abstract class Element {
   def contents: Array[String] // tine continutul linie cu linie
   def height(): Int = contents.length
   def width: Int = if (contents.isEmpty) 0 else contents(0).length
   val name:String
-  def above(other: Element):Element = new ArrayElement(contents ++ other.contents)
+  def above(other: Element):Element = Element(contents ++ other.contents)
   def beside(other: Element):Element = {
     require(other.contents.length == contents.length)
     val shmecher = for ((a, b) <- contents zip other.contents) yield a + b
-    new ArrayElement(shmecher
-    )
+    Element(shmecher)
   }
 
   override def toString: String = contents.mkString("\n")
 }
 
-// override este OBLIGATORIU DACA suprascrii o metoda. Optional Daca o IMPLEMENTEZI
-class ArrayElement(initialContent: Array[String]) extends Element {
-  override val contents:Array[String] = uniformPad(initialContent)
-
-  def uniformPad(lines:Array[String]):Array[String] = {
-    val longestLine:String = lines.maxBy(_.length)
-    val maxLength:Int = longestLine.length
-
-    def padLine(line: String) = {
-      val halfNoSpaces = (maxLength - line.length) / 2
-      val halfPadding = " " * halfNoSpaces
-      halfPadding + line + halfPadding
-    }
-
-    for (line <- lines) yield padLine(line)
-  }
-
-  // implementing an abstract function fara () cu o variabila/valoare !!!!
-  // "Uniform access principle" inseamna ca implementarea unei fct abstract fara () poate fi un camp.
-  // DPDV al clientului nu ar trebui sa conteze
-  val name = "ArrayElem"
-  //def name = "ArrayElem" -- NU poti implementa un VAL ABSTRACT cu un DEF, dar poti implementa un DEF ABSTRACT cu un VAL
-}
-
-//new ArrayElement(Array("rosu","galben","albastru"))
-
-class LineElement(line:String) extends Element {
-  def contents = Array(line)
-  val name = "LineElem" // In scala final opreste override. Atat!
-  override def width = line.length
-  override def height = 1
-}
-
-class UniformElement(ch:Char, width:Int, height: Int) extends Element {
-  override def contents: Array[String] = Array.fill[String](height)(ch.toString * width)
-  override val name: String = "UniformElem"
-}
 
 object Chapter10_OOP extends App {
-  val primuElem = new ArrayElement(Array("abc"))
+  val primuElem = Element(Array("abc"))
 //  println(primuElem.contents()) // nu compileaza pt ca functia ai declarat-o FARA ()
 
   // DAR daca ai declarat-o cu (), poti sa o apelezi si CU si FARA :
@@ -73,15 +79,19 @@ object Chapter10_OOP extends App {
 
 //  println((new OClasaMica).i)
 
-  val ue = new UniformElement('*', 3, 2)
+  val ue = Element('*', 3, 2)
   println(ue)
 
-  val steagRO = new LineElement("rosu") above new LineElement("galben") above new LineElement("albastru")
+  val steagRO = Element("rosu") above Element("galben") above Element("albastru")
   println(steagRO)
-  val steagFR = new LineElement("rosu") above new LineElement("maro") above new LineElement("albastru")
+  val steagFR = Element("rosu") above Element("maro") above Element("albastru")
 
-  val steaguri = steagRO beside steagFR
+  val sep = Element('*',2,3)
+
+  val steaguri = steagRO beside sep beside steagFR
   println(steaguri)
+
+
 
   val strings = Array("rosu", "galben")
 
---
+++ course/src/Chapter11_TinyTipes.scala
@@ -0,0 +1,15 @@
+//class Dollar(val value:Float) // asta ar fi o sublclasa a lui AnyRef
+
+class Dollar(val value:Float) extends AnyVal {
+  override def toString: String = "$" + value
+
+}
+
+object Chapter11_TinyTipes extends App {
+
+  def imprumuta(bani:Dollar) = {
+    println("am imprumutat banii: " + bani)
+  }
+  imprumuta(new Dollar(10))
+
+}
