b105e789310d40092940d10a08a6adf08d5ca98f
Victor Rentea <victorrentea@gmail.com>
2017-08-03 10:54:30 +0300

    visibility private[Ceva}

--- course/src/rocket/beacon/Beacon.scala
+++ course/src/rocket/beacon/Beacon.scala
@@ -1,5 +1,6 @@
 package rocket.beacon
 
+
 import rocket.Rocket
 //import java.util.List;
 
@@ -22,5 +23,23 @@ class Beacon {
 
     val ok2 = "\\d{10}".r.findAllMatchIn("1234567890")
   }
+
+  def metoda2 = {
+    import java.util.regex //in Scala poti importa pachete intregi
+    regex.Pattern.compile("\\d") // din simbolul regex mergi mai departe
+  }
+
+  def javaUtilDateERau = {
+    //import java.util.Date identic cu:
+    //import java.util.{Date}
+    import java.util.{Date => BadDate}
+    val dataRea = new BadDate()
+  }
+
+  def interzisJavaUtilDateDeTot = {
+    import java.util.{Date => _} // ascund o clasa dintr-un pachet..
+    val dataRea = new Date()
+  }
+
   new Rocket
 }
