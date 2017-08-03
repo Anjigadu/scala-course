fedb1a5e1027ae3c7032b3e5bc17d53d7f6e5a70
Victor Rentea <victorrentea@gmail.com>
2017-08-03 11:12:15 +0300

    MapperUtils in acelasi pachet

---
+++ course/src/SpringDataJpaRepositoryPlay.scala
@@ -0,0 +1,14 @@
+//trait Entity[ID] {
+//  def id: ID
+//}
+//trait JpaRepo[E[ID],ID] {
+//  def getById(id:ID):E
+//}
+//class Employee(var id:Long) extends Entity[Long] {
+//
+//}
+//trait EmployeeRepo extends JpaRepo[Employee, Long]
+//
+//class SpringDataJpaRepositoryPlay {
+//
+//}
--- course/src/mapper/UserMapper.scala
+++ course/src/mapper/UserMapper.scala
@@ -6,23 +6,24 @@ import java.util.Date
 class User {
   val date:Date = new Date()
 }
-
 class UserDto {
   var creationDate:String = _
-
 }
-
 object UserMapper extends App {
 
   def toDto(entity: User): UserDto = {
     val dto = new UserDto
-    dto.creationDate = dateToString(entity.date) //de formatat
+    dto.creationDate = MapperUtils.dateToString(entity.date) //de formatat
     dto
   }
 
-  private def dateToString(date: Date) = {
+
+  println(toDto(new User).creationDate)
+}
+
+object MapperUtils {
+  def dateToString(date: Date) = {
     new SimpleDateFormat("yyyy-MM-dd").format(date)
   }
 
-  println(toDto(new User).creationDate)
 }
