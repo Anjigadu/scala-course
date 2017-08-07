6d103537174c9ba01f65f956517498be277873a5
Victor Rentea <victorrentea@gmail.com>
2017-08-07 16:08:48 +0300

    factor out presentation vs logic

--- course/src/PrimeNumbers.scala
+++ course/src/PrimeNumbers.scala
@@ -1,7 +1,7 @@
 object PrimeNumbers extends App {
 
 
-  def isPrim(num: Int): Boolean = {
+  private def isPrim(num: Int): Boolean = {
     def isPrimeVsNrFrom(div: Int): Boolean = {
       if (div >= num) true
       else if (num % div == 0) false
@@ -10,14 +10,19 @@ object PrimeNumbers extends App {
     isPrimeVsNrFrom(2)
   }
 
+  private def nrPrimePanaLa(num: Int) = {
+    (2 to num).filter(isPrim)
+  }
 
   def nrPrime(num: Int): Int = {
-    val numbers = (2 to num).filter(isPrim)
+    val numbers = nrPrimePanaLa(num)
     numbers.foreach(println)
     val count  = numbers.length
     println(s"Pana la $num sunt $count numere prime.")
     count
   }
+
+
 
   nrPrime(10000)
 
